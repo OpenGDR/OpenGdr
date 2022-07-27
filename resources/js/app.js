ecd0b96ba6b3116eb615d9e66e19f31b4f3b5ff0
Mattia Bassi <mattia.bassi@mazzmedia.com>
2022-07-27 16:21:06 +0200

    aggiunta base del frontend con Vue, Vuex e Bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 import './bootstrap';
+import { createApp } from 'vue';
+
+import App from './components/App.vue'
+import router from './router'
+import axios from 'axios'
+
+/**
+ * Next, we will create a fresh Vue application instance. You may then begin
+ * registering components with the application instance so they are ready
+ * to use in your application's views. An example is included for you.
+ */
+
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+app.use(router);
+app.mount('#app');
---
+++ resources/js/router/index.js
@@ -0,0 +1,42 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+import Register from '../pages/Register.vue';
+import Login from '../pages/Login.vue';
+import Dashboard from '../pages/Dashboard.vue';
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'about',
+        path: '/about',
+        component: About
+    },
+    {
+        name: 'register',
+        path: '/register',
+        component: Register
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: Login
+    },
+    {
+        name: 'dashboard',
+        path: '/dashboard',
+        component: Dashboard
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+});
+
+export default router;
